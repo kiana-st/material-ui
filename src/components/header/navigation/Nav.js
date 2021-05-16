164978397b7fa1df1e441275c9f5b3550e49ef0d
kiana <soltany.2019@gmail.com>
2021-05-16 16:42:52 +0430

    nav finish

--- src/components/header/navigation/Nav.js
+++ src/components/header/navigation/Nav.js
@@ -12,25 +12,26 @@ import {
 } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-  accountButton:{
-    marginLeft:'auto',
-    '&:hover':{
-      background:'red',
+  accountButton: {
+    marginRight: theme.spacing(2),
+
+    '&:hover': {
+      background: 'red',
     },
   },
-  nav:{
-  }
+  nav: {
+    marginLeft: 'auto',
+  },
 }));
 
 function Nav() {
- 
   const classes = useStyles();
 
   const [anchorEl, setAnchorEl] = useState(null);
 
   const [value, setValue] = useState(0);
 
-  const handleOpenMenu = e => {
+  const handleOpenMenu = (e) => {
     setAnchorEl(e.currentTarget);
   };
   const handleMenuClose = () => {
@@ -51,27 +52,30 @@ function Nav() {
             indicatorColor="secondary"
             value={value}
             className={classes.nav}
-
           >
             <Tab disableRipple label="Home" />
             <Tab disableRipple label="Services" />
             <Tab disableRipple label="Products" />
           </Tabs>
-          <Button 
-          className={classes.accountButton}
-          aria-controls="menu"
-          onClick={handleOpenMenu}
-          disableRipple 
-          variant="outlined" 
-          color="secondary">
+          <Button
+            className={classes.accountButton}
+            aria-controls="menu"
+            onClick={handleOpenMenu}
+            disableRipple
+            variant="outlined"
+            color="secondary"
+          >
             SIGN UP
           </Button>
         </Toolbar>
       </AppBar>
-      <Menu 
-      style={{marginTop:'50px'}}
-      id="menu" anchorEl={anchorEl}  open={Boolean(anchorEl)}
-        onClose={handleMenuClose}>
+      <Menu
+        style={{ marginTop: '50px' }}
+        id="menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+      >
         <MenuList onClick={handleMenuClose}>My Account</MenuList>
         <MenuList onClick={handleMenuClose}>Exit</MenuList>
       </Menu>
