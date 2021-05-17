43b7f7264d7dd27e4c653e7782d323c8e9046410
Second <soltany.2019@gmail.com>
2021-05-18 02:05:08 +0430

    commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,13 @@ function Header() {
     return (
         <div className="container">
             <Nav />
-            <video src=""/>
+            <video src="/public/videos/video-2.mp4"/>
+            <h1>ADVENTURE AWAITING</h1>
+            <p>WHAT ARE OU WAITING FOR?</p>
+            <div>
+                <button>GET STARTED</button>
+                <button>WATCH TRAILER</button>
+            </div>
         </div>
     )
 }
